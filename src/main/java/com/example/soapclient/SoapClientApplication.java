b6c98a8c80932749603664835d052a85d91dffdc
rahmanz11 <zillur.babu@gmail.com>
2022-09-26 11:23:00 +0600

    logged

--- src/main/java/com/example/soapclient/SoapClientApplication.java
+++ src/main/java/com/example/soapclient/SoapClientApplication.java
@@ -16,6 +16,11 @@ public class SoapClientApplication {
 	BridgeClient client;
 
 	public static void main(String[] args) {
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
 		SpringApplication.run(SoapClientApplication.class, args);
 	}
 
